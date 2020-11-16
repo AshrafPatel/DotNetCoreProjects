540fead23a61dffc88bb62cb5b56e4f0000bd981
Ashraf Patel <4shr4f@live.co.uk>
2020-11-16 00:18:07 +0000

    Added ConnectionString to SongsList project

---
+++ SongList/SongList/Models/SongContext.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SongList.Models
+{
+    public class SongContext : DbContext
+    {
+        public SongContext(DbContextOptions<SongContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Song> Songs { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Song>().HasData(
+                new Song
+                {
+                    SongId = 1,
+                    Name = "Take Control",
+                    Year = 2020,
+                    Rating = 5
+                },
+                new Song
+                {
+                    SongId = 2,
+                    Name = "Dr. Stone",
+                    Year = 2019,
+                    Rating = 3
+                }
+            );
+        }
+    }
+}

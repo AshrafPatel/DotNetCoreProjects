50d2c1377228640cab6d453f7c9c903df437f059
Ashraf Patel <4shr4f@live.co.uk>
2020-11-30 23:52:36 +0000

    Added TodoApp Finished Models and Connection String

---
+++ TodoListApp/TodoListApp/Models/Filters.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoListApp.Models
+{
+    public class Filters
+    {
+        public string FilterString { get; set; }
+        public string CategoryId { get; set; }
+        public string DueDate { get; set; }
+        public string StatusId { get; set; }
+        public Filters(string filterString)
+        {
+            FilterString = filterString ?? "all-all-all";
+            string[] filters = FilterString.Split("-");
+            CategoryId = filters[0];
+            DueDate = filters[1];
+            StatusId = filters[2];
+        }
+
+        public bool HasCategoryFilter => CategoryId.ToLower() != "all";
+        public bool HasDueDateFilter => CategoryId.ToLower() != "all";
+        public bool HasStatusFilter => CategoryId.ToLower() != "all";
+
+        public static Dictionary<string, string> DueDateFilterValues =>
+            new Dictionary<string, string>
+            {
+                {"future", "Future" },
+                {"past", "Past" },
+                {"today", "Today" }
+            };
+
+        public bool IsPast => DueDate.ToLower() == "past";
+        public bool IsFuture => DueDate.ToLower() == "future";
+        public bool IsToday => DueDate.ToLower() == "today";
+    }
+}
---
+++ TodoListApp/TodoListApp/Models/Task.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoListApp.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Please enter a Description")]
+        public string Description { get; set; }
+        
+        [Required(ErrorMessage = "Please enter a Due Date")]
+        public DateTime DueDate { get; set; }
+
+        [Required(ErrorMessage = "Please select a Category")]
+        public string CategoryId { get; set; }
+        public Category Category { get; set; }
+
+        [Required(ErrorMessage = "Please select a Status")]
+        public string StatusId { get; set; }
+        public Status Status { get; set; }
+
+        public bool Overdue => DateTime.Today > DueDate && StatusId == "open";
+    }
+}

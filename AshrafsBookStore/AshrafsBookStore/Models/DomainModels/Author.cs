ca2bf17e85665bc8b9b258658db4276970242107
Ashraf Patel <4shr4f@live.co.uk>
2021-01-18 09:49:59 +0000

    Initial Comit AshrafsBookStore App

---
+++ AshrafsBookStore/AshrafsBookStore/Models/DomainModels/Author.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AshrafsBookStore.Models.DomainModels
+{
+    public class Author
+    {
+        public int AuthorId { get; set; }
+        [Required(ErrorMessage = "Please enter First Name")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage ="Please enter Last Name")]
+        public string LastName { get; set; }
+        public string FullName => $"{FirstName} {LastName}";
+
+    }
+}
---
+++ AshrafsBookStore/AshrafsBookStore/Startup.cs
@@ -0,0 +1,70 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace AshrafsBookStore
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRouting(opt => opt.LowercaseUrls = true);
+            services.AddMemoryCache();
+            services.AddSession();
+            services.AddControllersWithViews().AddNewtonsoftJson();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseDeveloperExceptionPage();
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseSession();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapAreaControllerRoute(
+                    name: "admin",
+                    areaName: "Admin",
+                    pattern: "Admin/{Controller=Book}/{action=Index}/{id?}"
+                );
+
+                //Route For Paging Sorting and Filtering
+                endpoints.MapControllerRoute(
+                    name: "",
+                    pattern: "{Controller}/{action}/page/{pagenumber}/size/{pagesize}/sort/{sortfield}/filter/{author}/{genre}/{price}"
+                );
+
+                //Route For Paging and Sorting
+                endpoints.MapControllerRoute(
+                    name: "",
+                    pattern: "{Controller}/{action}/page/{pagenumber}/size/{pagesize}/sort/{sortfield}"
+                );
+
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}/{slug?}");
+            });
+        }
+    }
+}
